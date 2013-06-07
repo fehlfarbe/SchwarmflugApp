eed2ed296df56c538531dc6519b6700af297f663
Brakebein <bruschie@hotmail.com>
2013-06-07 19:10:34 +0200

    Google Maps klappt :))))

--- html/js/swarmmap.js
+++ html/js/swarmmap.js
@@ -6,28 +6,34 @@
 var myLocation;	// coordinates
 var mapOptions;
 var map;
-var googleMapsState = "";
 
 
 function loadGoogleMaps() {
-	googleMapsState = "loading";
-
-//function initMap() {
-	alert("initMap called");
+	
+	console.log("loadGoogleMaps() called");
+	
 	//myLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-	//myLoaction = new google.maps.LatLng(-34.397, 150.644);
-	/*mapOptions = {	center: myLocation,
-					zoom: 15,
-					mapTypeId: google.maps.MapTypeId.ROADMAP };
-    
-    map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);*/
-    
-    $('#map_canvas').gmap({'center': new google.maps.LatLng(-34.397, 150.644),
-    	'zoom': 15,
-    	'mapTypeControl': true,
-    	'navigationControl': true,
-    	'navigationControlOptions': {'position': google.maps.ControlPosition.LEFT_TOP}
+	try {
+		/*myLoaction = new google.maps.LatLng(-34.397, 150.644);
+		mapOptions = {	center: myLocation,
+						zoom: 15,
+						mapTypeId: google.maps.MapTypeId.ROADMAP };
+	    
+	    map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+	    */
+		$('#map_canvas').gmap({
+    		'center': new google.maps.LatLng(51.042, 13.727),
+    		'zoom': 15,
+    		'mapTypeControl': true,
+    		'callback': function(){console.log("callback klappt?");}
     	});
-    
-    //google.maps.event.trigger(map, 'resize');
+		
+		$('#map_canvas').gmap('option', 'streetViewControl', 'false');
+		$('#map_canvas').gmap('option', 'zoomControl', 'true');
+		$('#map_canvas').gmap('option', 'zoomControlOptions', { 'style': google.maps.ZoomControlStyle.SMALL, 'position': google.maps.ControlPosition.TOP_LEFT });
+	
+	} catch(e) {
+		console.log("error occured with google maps" + e);
+	}
+	
 }
