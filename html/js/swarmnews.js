708810ab3781f4d0b7e1e5bfa8cf996e374152b8
theDyingMountain <mario_gates@ymail.com>
2013-06-28 00:54:43 +0200

    fix news fehlerhaftes design

--- html/js/swarmnews.js
+++ html/js/swarmnews.js
@@ -9,6 +9,7 @@ function getSwarmNews() {
 			for(var i=0;  i < l; i++) {
 				appendNews(data.swarms[i]);
 			}
+			$('#swarmnews').listview('refresh');
 		},
 		error: function(xhRequest, ErrorText, thrownError) { console.log('getSwarms error ' +xhRequest.status+'|'+xhRequest.responseText); }
 	});
@@ -18,8 +19,8 @@ function appendNews(swarm) {
 	var ncontent = '<li id="snews"><h1>' + swarm.genus + ' ' + swarm.species + '</h1>';
 	ncontent += '<p>' + swarm.date + ' &ndash; Lat: ' + runde(swarm.position[0], 3) + ' / Lng: ' + runde(swarm.position[1], 3) + '</p>';
 	if(swarm.image) ncontent += '<img src="data:image/jpeg;base64,' + swarm.image + '" />';
-	if(swarm.comment) ncontent += 'Kommentar:' + swarm.comment;
-	ncontent += '</li>';
+	if(swarm.comment) ncontent += '<h3>Kommentar:' + swarm.comment;
+	ncontent += '</h3></li>';
 	
 	$('#swarmnews').append(ncontent);
 }
